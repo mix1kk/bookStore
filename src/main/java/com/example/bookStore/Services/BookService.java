c1f96111fbacbbb2e3f85e80dec32fa22cbc9f00
mix1kk <mix100@inbox.ru>
2023-08-13 14:19:48 +0500

    added CRUD and exceptions to services

--- src/main/java/com/example/bookStore/Services/BookService.java
+++ src/main/java/com/example/bookStore/Services/BookService.java
@@ -1,4 +1,50 @@
 package com.example.bookStore.Services;
 
+import com.example.bookStore.Models.Author;
+import com.example.bookStore.Models.Book;
+import com.example.bookStore.Repositories.BookRepository;
+import com.example.bookStore.Utils.BookExceptions.BookNotDeletedException;
+import com.example.bookStore.Utils.BookExceptions.BookNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    public Book findById(int bookId) {
+        return bookRepository.findById(bookId).orElseThrow(BookNotFoundException::new);
+    }
+
+    public void save(Book book) {
+        bookRepository.save(book);
+    }
+
+    public void update(int bookId, Book book) {
+        book.setBookId(bookId);
+        bookRepository.save(book);
+    }
+
+    public void delete(int bookId) {
+        if (!bookRepository.existsById(bookId)) {
+            throw new BookNotDeletedException("Книги не существует");
+        }
+        bookRepository.deleteById(bookId);
+        if (bookRepository.existsById(bookId)) {
+            throw new BookNotDeletedException("Не удалось удалить книгу");
+        }
+    }
 }
---
+++ src/main/java/com/example/bookStore/Utils/AuthorExceptions/AuthorNotFoundException.java
@@ -0,0 +1,4 @@
+package com.example.bookStore.Utils.AuthorExceptions;
+
+public class AuthorNotFoundException extends RuntimeException {
+}

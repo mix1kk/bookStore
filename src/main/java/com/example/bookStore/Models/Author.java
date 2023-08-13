693c7e721c1fcefbef805d95ad21afea5f341ad6
mix1kk <mix100@inbox.ru>
2023-08-13 14:56:08 +0500

    added schemas for swagger

--- src/main/java/com/example/bookStore/Models/Author.java
+++ src/main/java/com/example/bookStore/Models/Author.java
@@ -1,5 +1,6 @@
 package com.example.bookStore.Models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,14 +11,24 @@ import org.springframework.stereotype.Component;
 @Table(name = "author")
 @Getter
 @Setter
+@Schema(description = "Сущность автора")
 public class Author {
     @Id
     @Column(name = "\"authorId\"")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Идентификатор автора", example = "0")
     private int authorId;
     @Column(name = "\"authorName\"")
+    @Schema(description = "Имя автора", example = "Иван")
     private String authorName;
-    //books as string books id's (format: "bookId1_bookId2_..._bookIdn")
-    @Column(name = "\"books\"")
-    private String books;
+    @Column(name = "\"authorSurname\"")
+    @Schema(description = "Фамилия автора", example = "Иванов")
+    private String authorSurname;
+    @Column(name = "\"authorMiddlename\"")
+    @Schema(description = "Отчество автора", example = "Иванович")
+    private String authorMiddlename;
+
+//    //books as string books id's (format: "bookId1_bookId2_..._bookIdn")
+//    @Column(name = "\"books\"")
+//    private String books;
 }
